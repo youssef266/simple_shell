469ccbb260754bd51a8b1e692294872b0b2e804f
Karim-Aboualenin <karim72727272@yahoo.com>
2023-10-07 19:15:23 +0300

    AUTHORS, includes in shell intial version of main

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+ * main: main function to compile the code
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+return (0);
+}
---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+#endif
