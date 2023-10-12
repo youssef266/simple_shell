e296c03bc0685c07644c238a50d738d7d2ea9339
youssef266 <110091209+youssef266@users.noreply.github.com>
2023-10-12 14:49:46 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -15,8 +15,7 @@
 int interactive(char **argv, char **env);
 int non_interactive(int argc, char**);
 int _strtok(char **tok, char *line, char *delim);
-void signint(void);
+void signint(int sig);
 size_t _strlen(char *s);
 void handling_error(char *process, char *command);
-
 #endif
