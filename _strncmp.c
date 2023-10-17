0e9eaca578634d132bd78c33e0d80050e8b998b1
Karim-Aboualenin <karim72727272@yahoo.com>
2023-10-18 02:40:33 +0300

    _strncmp

---
+++ _strncmp.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+
+/**
+ * _strncmp - function that compares two strings.
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 0 if equal 1 if not
+*/
+int _strncmp(char *s1, char *s2, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+if (*(s1 + i) == *(s2 + i))
+{}
+else
+return (1);
+i++;
+}
+return (0);
+}
--- shell.h
+++ shell.h
@@ -48,5 +48,6 @@ char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
 int _atoi(char *s);
 int _isdigit(char *s);
+int _strncmp(char *s1, char *s2, int n);
 
 #endif
